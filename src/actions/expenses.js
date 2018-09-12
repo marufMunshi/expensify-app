6090eaed870446e81f8f895facdcd94495583964
marufMunshi <marufhasanmunshi@gmail.com>
2018-09-12 22:05:08 +0600

    Fetching data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -40,3 +40,28 @@ export const editExpense = (id, updates) => ({
     id: id,
     updates: updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => {
+    return {
+        type: 'SET_EXPENSES',
+        expenses
+    };
+};
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value', (snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                })
+            });
+
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
